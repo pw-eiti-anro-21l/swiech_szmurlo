161286b3973924c68105ad54ed9cb5a9fb738ef0
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-03-25 11:54:54 +0100

    wczytywanie parametrow z yaml

--- turtle_control/launch/turtle_control_launch.py
+++ turtle_control/launch/turtle_control_launch.py
@@ -2,25 +2,22 @@ import os
 from launch import LaunchDescription
 import launch_ros.actions
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     proc_env = os.environ.copy()
     proc_env['PYTHONUNBUFFERED'] = '1'
+    config = os.path.join('src','swiech_szmurlo','turtle_control','config', 'turtle_key_control_node.yaml')
     return LaunchDescription([
         Node(
             package='turtle_control',
             executable='turtle_control_talker',
-            name='custom_parameter_node',
+            name='turtle_key_control_node',
             prefix = "gnome-terminal --", 
             # env = proc_env,
             output='screen',
             emulate_tty=True,
-            parameters=[
-                {'up_key': 'h'},
-                {'down_key': 's'},
-                {'left_key': 'a'},
-                {'right_key': 'd'}
-            ]
+            parameters=[config]
         ),
         Node(
             package='turtlesim',
--- turtle_control/setup.py
+++ turtle_control/setup.py
@@ -18,8 +18,8 @@ setup(
     zip_safe=True,
     maintainer='piotr',
     maintainer_email='piotr.szmurlo7@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Turtlesim teleop',
+    license='free',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
