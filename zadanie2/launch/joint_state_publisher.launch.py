e536ae0a2b4156620697a3c533628ba59d7fa344
piotrszmurlo <80161486+piotrszmurlo@users.noreply.github.com>
2021-04-08 17:31:40 +0200

    jeden wystarczy

--- zadanie2/launch/joint_state_publisher.launch.py
+++ zadanie2/launch/joint_state_publisher.launch.py
@@ -21,11 +21,6 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher'),
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
