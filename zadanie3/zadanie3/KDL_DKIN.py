4df3658104ed01c8ec17387964613a001621f13a
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-04-22 18:49:10 +0200

    kdl chyba working

--- zadanie3/zadanie3/KDL_DKIN.py
+++ zadanie3/zadanie3/KDL_DKIN.py
@@ -12,21 +12,15 @@ class KDL_DKIN(Node):
 
     def __init__(self):
         super().__init__('KDL_DKIN')
-        self.publisher_ = self.create_publisher(PoseStamped, 'kdl_pose', 10)
+        self.publisher_ = self.create_publisher(PoseStamped, 'kdl_fk', 10)
         self.subsciber = self.create_subscription(JointState, 'joint_states', self.listener_callback, 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-
-    def timer_callback(self):
-        # msg = 0
-        # self.publisher_.publish(msg)
-        # self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.chain = self.build_chain()
         
     def listener_callback(self, msg):
-        pass
-    
+        pose = self.solve_forward_kinematics(self.chain, msg, 0.2)
+        self.publisher_.publish(pose)
+
     def get_xyz_rpy(self):
         with open("dh_params.json", "r") as file:
             dh_params = json.load(file)
@@ -54,44 +48,48 @@ class KDL_DKIN(Node):
         params = self.get_xyz_rpy()
         chain = kdl.Chain()
         joint_base_1 = kdl.Joint(kdl.Joint.TransZ)
-        frame0 = kdl.Frame(kdl.Rotation.RPY(params[0]['rpy'][0],params[0]['rpy'][1], params[0]['rpy'][2]), kdl.Vector(params[0]['xyz'][0],params[0]['xyz'][1], params[0]['xyz'][2]))
-        segment1 = kdl.Segment(joint_base_1,frame0)
-        chain.addSegment(segment1)
-
         joint_1_2 = kdl.Joint(kdl.Joint.TransZ)
-        frame1 = kdl.Frame(kdl.Rotation.RPY(params[1]['rpy'][0],params[1]['rpy'][1], params[1]['rpy'][2]), kdl.Vector(params[1]['xyz'][0],params[1]['xyz'][1], params[1]['xyz'][2]))
-        segment2 = kdl.Segment(joint_1_2,frame1)
-        chain.addSegment(segment2)
-
         joint_2_3 = kdl.Joint(kdl.Joint.TransZ)
+
+        frame0 = kdl.Frame(kdl.Rotation.RPY(params[0]['rpy'][0],params[0]['rpy'][1], params[0]['rpy'][2]), kdl.Vector(params[0]['xyz'][0],params[0]['xyz'][1], params[0]['xyz'][2]))
+        frame1 = kdl.Frame(kdl.Rotation.RPY(params[1]['rpy'][0],params[1]['rpy'][1], params[1]['rpy'][2]), kdl.Vector(params[1]['xyz'][0],params[1]['xyz'][1], params[1]['xyz'][2]))
         frame2 = kdl.Frame(kdl.Rotation.RPY(params[2]['rpy'][0],params[2]['rpy'][1], params[2]['rpy'][2]), kdl.Vector(params[2]['xyz'][0],params[2]['xyz'][1], params[2]['xyz'][2]))
+        
+        segment1 = kdl.Segment(joint_base_1,frame0)
+        segment2 = kdl.Segment(joint_1_2,frame1)
         segment3 = kdl.Segment(joint_2_3,frame2)
+        
+        chain.addSegment(segment1)
+        chain.addSegment(segment2)
         chain.addSegment(segment3)
+        return chain
 
     def solve_forward_kinematics(self, chain, msg, tool_length):
-        fk_result = kdl.ChainFkSolverPos_recursive(chain)
+        pose = PoseStamped()
+        fk_solver = kdl.ChainFkSolverPos_recursive(chain)
         result_frame = kdl.Frame()
         joint_states = kdl.JntArray(3)
         joint_states[0] = msg.position[0]
         joint_states[1] = msg.position[1]
         joint_states[2] = msg.position[2]
-        fk_result.JntToCart(joint_states, result_frame)
-        frame_orientation = result_frame.M.GetQuaternion()
+        fk_solver.JntToCart(joint_states, result_frame)
+        frame_quaternion = result_frame.M.GetQuaternion()
         tool_position = result_frame.p + tool_length
+        pose.pose.position.x = tool_position[0]
+        pose.pose.position.y = tool_position[1]
+        pose.pose.position.z = tool_position[2]
+        pose.pose.orientation = frame_quaternion
+        return pose
 
 def main(args=None):
 
     rclpy.init(args=args)
 
-    minimal_publisher = KDL_DKIN()
-    params = minimal_publisher.get_xyz_rpy()
-    print((params[0]['rpy'][0],params[0]['rpy'][1], params[0]['rpy'][2]), (params[0]['xyz'][0],params[0]['xyz'][1], params[0]['xyz'][2]))
-    rclpy.spin(minimal_publisher)
+    kdl_fk = KDL_DKIN()
+    print(kdl_fk.chain)
+    rclpy.spin(kdl_fk)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    kdl_fk.destroy_node()
     rclpy.shutdown()
 
 
