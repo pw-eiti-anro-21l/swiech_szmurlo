541ec804ba072cde01226614562ee6e00d398b1f
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-05-05 10:57:46 +0200

    interface oackage, service added

--- zadanie4/zadanie4/jint.py
+++ zadanie4/zadanie4/jint.py
@@ -1,14 +1,45 @@
 import sys
-
 from example_interfaces.srv import AddTwoInts
 import rclpy
 from rclpy.node import Node
-from '../srv' import request_message
+from zadanie4_interface.srv import Interpolation
 
-class jint (Node):
+class jint(Node):
     def __init__(self):
+        self.client = self.create_client(Interpolation, 'interpolation')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = Interpolation.Request()
 
-        self.subscription = self.create_subscription(JointState, 'joint_states', self.joint_position_callback, 10)
-        self.service
-        self.joint_start_position = [0, 0, 0]
-        
+    def send_request(self):
+        self.req.position_joint1 = int(sys.argv[1])
+        self.req.position_joint2 = int(sys.argv[2])
+        self.req.position_joint3 = int(sys.argv[3])
+        self.req.interpolation_time = int(sys.argv[4])
+        self.req.method = sys.argv[5]
+        self.future = self.client.call_async(self.req)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    jint_client = jint()
+    jint_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(jint_client)
+        if jint_client.future.done():
+            try:
+                response = jint_client.future.result()
+            except Exception as e:
+                jint_client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                jint_client.get_logger().info("donezo")
+            break
+
+    jint_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- zadanie4/zadanie4/jint_control_srv.py
+++ zadanie4/zadanie4/jint_control_srv.py
@@ -1,27 +1,43 @@
 import rclpy
 from rclpy.node import Node
-from '../srv' import request_message
-
+from example_interfaces.srv import AddTwoInts
+from zadanie4_interface.srv import Interpolation
+from sensor_msgs.msg import JointState
+from math import floor
+from rclpy.qos import QoSProfile
+from time import sleep
 
 class InterpolationServer(Node):
 
     def __init__(self):
         super().__init__('interpolation_server')
-        self.srv = self.create_service(request_message, 'interpolation', self.interpolation_callback)
-
+        self.srv = self.create_service(Interpolation, 'interpolation', self.interpolation_callback)
+        qos_profile = QoSProfile(depth=10)
+        self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
     def interpolation_callback(self, request, response):
-
-        solve_linear_interpolation()
+        if request.interpolation_time <= 0:
+            response.server_feedback = "time must be greater than 0"
+        self.linear_interpolation(request)
 
         response.server_feedback = "Interpolation completed"
 
-        self.get_logger().info(f'Incoming request {position_joint1}, {position_joint2}, {position_joint3}, {interpolation_time}')
+        # self.get_logger().info(f'Incoming request {position_joint1}, {position_joint2}, {position_joint3}, {interpolation_time}')
 
         return response
     
-    def solve_linear_interpolation():
-        joint_positions = [0, 0, 0]
-        
+
+    def linear_interpolation(self, request):
+        sample_time = 0.1
+        steps = floor(request.interpolation_time/sample_time)
+        joint_states = JointState()
+        start_joint_states = [0, 0, 0]
+        for step in range(steps + 1):
+            joint_1_state = start_joint_states[0] + (request.position_joint1 - start_joint_states[0])/steps*step
+            joint_2_state = start_joint_states[1] + (request.position_joint2 - start_joint_states[1])/steps*step
+            joint_3_state = start_joint_states[2] + (request.position_joint3 - start_joint_states[2])/steps*step
+            joint_states = [joint_1_state, joint_2_state, joint_3_state]
+            self.joint_pub.publish(joint_states)
+            sleep(sample_time)
 
 
 def main(args=None):
