d241fd9d3ce378f7a1d705e172a745b9bdaf798c
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-05-06 13:43:20 +0200

    urdf dodany, launch do rviza

--- zadanie4/setup.py
+++ zadanie4/setup.py
@@ -14,7 +14,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('srv/*.srv')),
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -25,8 +26,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	'service = zadanie4.jint_control_srv:main',
-        	'client = zadanie4.jint:main',
+        	'interpolation_service = zadanie4.jint_control_srv:main',
+        	'interpolation_client = zadanie4.jint:main',
         ],
     },
 )
---
+++ zadanie4/urdf/dh_transform.py
@@ -0,0 +1,160 @@
+import json
+import yaml
+import math
+from math import cos, sin, atan, atan2, sqrt
+# import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import xml
+from lxml import etree as ET
+import json
+import transformations
+import pprint
+
+def get_parameters(part):
+    with open("params_server.json", "r") as read_file:
+        data = json.load(read_file)
+    part_parameters = data[part]
+    return part_parameters
+
+
+def get_xyz_rpy():
+    with open("dh_params.json", "r") as file:
+        dh_params = json.load(file)
+    rpy_xyz={}
+    inter = 1
+    iterator = 1
+
+    xyz_array = []
+    rpy_array = []
+    params_array = []
+
+    for i in dh_params:
+        dh_row = json.loads(json.dumps(i))
+        a_translation = transformations.translation_matrix((dh_row["a"],0,0))
+        d_translation = transformations.translation_matrix((0,0,dh_row["d"]))
+        alpha_rotation = transformations.rotation_matrix(dh_row["alpha"],(1, 0, 0))
+        theta_rotation = transformations.rotation_matrix(dh_row["theta"],(0, 0, 1))
+        trans_matrix = a_translation @ alpha_rotation @ d_translation @ theta_rotation
+        rpy = transformations.euler_from_matrix(trans_matrix)
+        xyz = transformations.translation_from_matrix(trans_matrix)
+
+        params_array.append({'xyz': xyz, 'rpy': rpy, 'd': dh_row['d']})
+        xyz_array.append(xyz)
+        rpy_array.append(rpy)
+
+    return params_array
+
+
+
+def create_xml_link(roll, pitch, yaw, x, y, z, length, name):
+
+    parameters = get_parameters(name)
+
+    if name == 'base' or name == 'tool':
+        z_translation = str(float(0.5*length))
+
+    else:
+        z_translation = str(float(-0.5*length))
+    length = str(length)
+
+    link_name = parameters['link_name']
+    mass_value = parameters['mass']
+    xyz = f'{0} {0} {z_translation}'
+    rpy = f'{0} {0} {0}'
+    radius = parameters['radius']
+    material_name = parameters['material_name']
+    color_rgba = parameters['color']
+    mu = parameters['mu']
+    kp = parameters['kp']
+    kd = parameters['kd']
+
+
+    link = ET.Element('link', name = link_name)
+
+    visual = ET.SubElement(link, "visual")
+    origin_visual = ET.SubElement(visual, "origin", xyz = xyz, rpy = rpy)
+    geometry_visual = ET.SubElement(visual, "geometry")
+    cylinder_visual = ET.SubElement(geometry_visual, "cylinder", radius = radius, length = length)
+    material_visual = ET.SubElement(visual, "material", name = material_name)
+    color_visual = ET.SubElement(material_visual, "color", rgba = color_rgba)
+
+    collision = ET.SubElement(link, "collision")
+    origin_collision = ET.SubElement(collision, "origin", xyz = xyz, rpy = rpy)
+    geometry_collision = ET.SubElement(collision, "geometry")
+    cylinder_collision = ET.SubElement(geometry_collision, "cylinder", radius = radius, length = length)
+    contact_coefficients_collision = ET.SubElement(collision, "contact_coefficients", mu = mu, kp = kp, kd = kd)
+   
+    return link
+
+def create_xml_joint(roll, pitch, yaw, x, y, z, name):
+
+    parameters = get_parameters(name)
+
+    joint_name = parameters['joint_name']
+    joint_type = parameters['joint_type']
+    parent_link = parameters['parent']
+    child_link = parameters['child']
+    origin_xyz = f'{x} {y} {z}'
+    origin_rpy = f'{roll} {pitch} {yaw}'
+    axis_xyz = parameters['axis_xyz']
+    upper_limit = parameters['upper_limit']
+    lower_limit = parameters['lower_limit']
+    effort_limit = parameters['effort']
+    velocity_limit = parameters['velocity']
+
+    joint = ET.Element('joint', name = joint_name, type = joint_type)
+    parent = ET.SubElement(joint, 'parent', link = parent_link)
+    child = ET.SubElement(joint, 'child', link = child_link)
+    origin = ET.SubElement(joint, 'origin', xyz = origin_xyz, rpy = origin_rpy)
+    axis = ET.SubElement(joint, 'axis', xyz = axis_xyz)
+    limit = ET.SubElement(joint, 'limit', upper = upper_limit, lower = lower_limit, effort = effort_limit, velocity = velocity_limit)
+    
+    return joint
+
+
+if __name__ == "__main__":
+
+    params_array = get_xyz_rpy()
+    array_of_urdfs = []
+
+    joint_iterator = 1
+
+    for params in params_array:
+        rpy = params['rpy']
+        xyz = params['xyz']
+        d_translation = params['d']
+        roll = rpy[0]
+        pitch = rpy[1]
+        yaw = rpy[2]
+        x = xyz[0]
+        y = xyz[1]
+
+        if joint_iterator == 1:
+            z = xyz[2]+1 # podwyzszenie bazy
+        else:
+            z = xyz[2]
+
+        d = d_translation
+
+        joint_name_in_server = f'joint_{joint_iterator}'
+        link_name_in_server = f'link_{joint_iterator}'
+
+        joint_urdf = create_xml_joint(roll, pitch, yaw, x, y ,z, joint_name_in_server)
+        link_urdf = create_xml_link(roll, pitch, yaw, x, y, z, d_translation, link_name_in_server)
+        array_of_urdfs.append(joint_urdf)
+        array_of_urdfs.append(link_urdf)
+        joint_iterator += 1
+
+        # print (f'Roll: {roll}, Pitch: {pitch}, Yaw: {yaw} \n')
+    
+    base_urdf = create_xml_link(0, 0, 0, 0, 0, 0, 1, "base")
+    tool_urdf = create_xml_link(0, 0, 0, 0, 0, 0, 0.2, "tool")
+    body_tool_joint_urdf = create_xml_joint(0, 0, 0, 0, 0, 0, "joint_tool")
+
+    tree = ET.Element("robot", name = "my_robot")
+    tree.extend([base_urdf])
+    tree.extend(array_of_urdfs)
+    tree.extend([body_tool_joint_urdf])
+    tree.extend([tool_urdf])
+    ET.ElementTree(tree).write('my_robot.urdf.xml', pretty_print=True)
+    print("URDF generated to my_robot.urdf.xml")
