6a798d5a01225afd1ccd2c5a73c618cfc9b6e8d6
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-05-06 19:42:31 +0200

    funkcjonalnosc gotowa

--- zadanie4/zadanie4/oint_control_srv.py
+++ zadanie4/zadanie4/oint_control_srv.py
@@ -4,61 +4,44 @@ from zadanie4_interface.srv import OpInterpolation
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Quaternion
-from transforms3d.euler import euler2quat
-from math import floor, pi
 from rclpy.qos import QoSProfile
 from time import sleep
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
-
+from math import cos, sin, floor
 
 class OpInterpolationServer(Node):
 
     def __init__(self):
         super().__init__('op_interpolation_server')
         self.srv = self.create_service(OpInterpolation, 'op_interpolation', self.interpolation_callback)
-        
         qos_profile = QoSProfile(depth=10)
         self.pose_publisher = self.create_publisher(PoseStamped, 'oint_pose', qos_profile)
-
         self.initial_position = [0, 0, 0]
         self.initial_orientation = [0, 0, 0]
 
-        # self.subsciber = self.create_subscription(JointState, 'joint_states', self.listener_callback, 10)
-        self.in_action = False
-
-    # def listener_callback(self, msg):
-    #     if not self.in_action:
-    #         self.initial_position[0] = msg.position[0]
-    #         self.initial_position[1] = msg.position[1]
-    #         self.initial_position[2] = msg.position[2]
-
-
     def interpolation_callback(self, request, response):
-
-        self.in_action = True
 
         if request.interpolation_time <= 0:
             response.server_feedback = "Given interpolation time < 0. Defaulting to 5s"
             request.interpolation_time = 5
         
         if request.version != "std" and request.version != "ext":
-            response.server_feedback = "Given version is wrong. Defaulting to standart"
+            response.server_feedback = "Given version is wrong. Defaulting to standard"
             request.version = "std"
 
         if request.method != "linear" and request.method != "trapezoid":
             request.method = "linear"
-            self.get_logger().info("Given interpolation method is wrong. Defaulting to linearear")
+            self.get_logger().info("Given interpolation method is wrong. Defaulting to linear")
 
         if request.method == "linear":
-            pose = self.linear_interpolation(request)
+            self.linear_interpolation(request)
             response.server_feedback = "Interpolation completed"
 
         elif request.method == "trapezoid":
-            pose = self.trapezoid_interpolation(request)
+            self.trapezoid_interpolation(request)
             response.server_feedback = "Interpolation completed"
 
-        # self.pose_publisher.publish(pose)
         return response
     
 
@@ -67,11 +50,8 @@ class OpInterpolationServer(Node):
         sample_time = 0.01
         steps = floor(request.interpolation_time/sample_time)
         pose = PoseStamped()
-
         initial_position = self.initial_position
         initial_orientation = self.initial_orientation
-
-
         for step in range(steps + 1):
 
             pose_x = initial_position[0] + (request.x_goal - initial_position[0])/steps*step
@@ -83,34 +63,21 @@ class OpInterpolationServer(Node):
             ort_yaw = initial_orientation[2] + (request.yaw_goal - initial_orientation[2])/steps*step
             ort_quaternion = Quaternion(w=0.0, x=ort_roll, y=ort_pitch, z=ort_yaw)
 
-            roll_rad = ort_roll * pi/180
-            pitch_rad = ort_pitch * pi/180
-            yaw_rad = ort_yaw * pi/180
-
-            quat = euler2quat (roll_rad, pitch_rad, yaw_rad)
-            
-            if request.version == "ext":
-                ort_quaternion = Quaternion(w=0.0, x=roll_rad, y=pitch_rad, z=yaw_rad)
+            if request.version == "ext":  
+                ort_quaternion = self.euler_to_quaternion(ort_roll, ort_pitch, ort_yaw)
             else:
-                ort_quaternion = Quaternion(w=quat[0], x=quat[1], y=quat[2], z=quat[3])
-
+                ort_quaternion = self.euler_to_quaternion(0, 0, 0)
             pose.header.frame_id = "base_link"
             pose.pose.position.x = pose_x
             pose.pose.position.y = pose_y
             pose.pose.position.z = pose_z
             pose.pose.orientation = ort_quaternion
-
             sleep(sample_time)
             
             self.pose_publisher.publish(pose)
 
         self.initial_position = [pose_x, pose_y, pose_z]
         self.initial_orientation = [ort_roll, ort_pitch, ort_yaw]
-
-            # return pose
-
-        # self.initial_joint_states = [joint_1_state, joint_2_state, joint_3_state]
-
 
     def trapezoid_interpolation(self, request):
         sample_time = 0.01
@@ -181,17 +148,11 @@ class OpInterpolationServer(Node):
             ort_roll = ort_roll + (last_vel_roll + curr_vel_roll) * request.interpolation_time/steps
             ort_pitch = ort_pitch + (last_vel_pitch + curr_vel_pitch) * request.interpolation_time/steps
             ort_yaw = ort_yaw + (last_vel_yaw + curr_vel_yaw) * request.interpolation_time/steps
-            
-            roll_rad = ort_roll * pi/180
-            pitch_rad = ort_pitch * pi/180
-            yaw_rad = ort_yaw * pi/180
 
-            quat = euler2quat (roll_rad, pitch_rad, yaw_rad)
-            
-            if request.version == "ext":
-                ort_quaternion = Quaternion(w=0.0, x=roll_rad, y=pitch_rad, z=yaw_rad)
+            if request.version == "ext":  
+                ort_quaternion = self.euler_to_quaternion(ort_roll, ort_pitch, ort_yaw)
             else:
-                ort_quaternion = Quaternion(w=quat[0], x=quat[1], y=quat[2], z=quat[3])
+                ort_quaternion = self.euler_to_quaternion(0, 0, 0)
 
             pose.header.frame_id = "base_link"
             pose.pose.position.x = pose_x
@@ -205,16 +166,19 @@ class OpInterpolationServer(Node):
         self.initial_position = [pose_x, pose_y, pose_z]
         self.initial_orientation = [ort_roll, ort_pitch, ort_yaw]
 
+    def euler_to_quaternion(self,roll, pitch, yaw):
+        qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+        qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+        qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+        qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+        return Quaternion(x=qx, y=qy, z=qz, w=qw)
+
 
 def main(args=None):
     rclpy.init(args=args)
-
     op_interpolation_server = OpInterpolationServer()
-
     rclpy.spin(op_interpolation_server)
-
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
