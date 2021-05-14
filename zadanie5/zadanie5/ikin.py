e918157c17fdbd25466ed9411d776915060a16dc
macieksww <macieksw2000@gmail.com>
2021-05-14 12:09:33 +0200

    z5

--- zadanie5/zadanie5/ikin.py
+++ zadanie5/zadanie5/ikin.py
@@ -2,13 +2,18 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
+from math import cos, sin, atan, atan2, sqrt
+from rclpy.qos import QoSProfile 
+import json
+import transformations
+import mathutils
 
 
 class IKIN(Node):
 
     def __init__(self):
         super().__init__('ikin')
-        self.srv = self.create_service(Pose, '/pose_stamped', self.service_callback)
+        self.srv = self.create_service(PoseStamped, '/pose_stamped', self.service_callback)
 
         qos_profile = QoSProfile(depth=10)
         self.joint_publisher = self.create_publisher(JointState, '/joint_states', qos_profile)
@@ -35,8 +40,48 @@ class IKIN(Node):
         joint_1_2_trans = -pose_y
         joint_2_3_trans = pose_x
 
-        joint_states.position = [float(joint_1_state), float(joint_2_state), float(joint_3_state)]
+        if joint_base_1_trans < -1 or joint_base_1_trans > 0:
+            raise Exception("Niemożliwe do zrealizowania położenie stawu base -> 1")
+        
+        if joint_1_2_trans < -1 or joint_1_2_trans > 0:
+            raise Exception("Niemożliwe do zrealizowania położenie stawu 1 -> 2")
+
+        if joint_2_3_trans < -1 or joint_2_3_trans > 0:
+            raise Exception("Niemożliwe do zrealizowania położenie stawu 2 -> 3")
+
+
+
+        joint_states.position = [float(joint_base_1_trans), float(joint_1_2_trans), float(joint_2_3_trans)]
         self.joint_pub.publish(joint_states)
+
+
+
+    def get_xyz_rpy(self, msg):
+        with open("/home/piotr/anro1_ws/src/swiech_szmurlo/zadanie3/zadanie3/dh_params.json", "r") as file:
+            dh_params = json.load(file)
+            xyz_array = []
+            rpy_array = []
+            params_array = []
+            trans_matrix_list = []
+            msg_pos_it = 0
+            for i in dh_params:
+                dh_row = json.loads(json.dumps(i))
+                a_translation = mathutils.Matrix.Translation((dh_row["a"], 0, 0))
+                d_translation = mathutils.Matrix.Translation((0, 0, dh_row["d"]+msg.position[msg_pos_it]))
+                alpha_rotation = mathutils.Matrix.Rotation (dh_row["alpha"], 4, 'X')
+                theta_rotation = mathutils.Matrix.Rotation (dh_row["theta"], 4, 'Z')
+                trans_matrix = a_translation @ alpha_rotation @ d_translation @ theta_rotation
+                rpy = transformations.euler_from_matrix(trans_matrix)
+                xyz = transformations.translation_from_matrix(trans_matrix)
+                params_array.append({'xyz': xyz, 'rpy': rpy, 'd': dh_row['d']})
+                xyz_array.append(xyz)
+                rpy_array.append(rpy)
+                trans_matrix_list.append(trans_matrix)
+                base_tool_matrix = trans_matrix_list[0]
+                for i in range(1, len(trans_matrix_list)):
+                    base_tool_matrix = base_tool_matrix @ trans_matrix_list[i]
+                msg_pos_it += 1
+            return base_tool_matrix
 
 
 def main(args=None):
